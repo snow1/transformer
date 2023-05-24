f453ecadd18b9a8263ab4a34273736a396e99fdc
snow1 <15653507629@163.com>
2023-05-24 21:08:42 +0200

    f

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/Example5/Markdown
-bsub -o "outputs/Example5/Markdown/Example5_0.md" -J "Example5_0" -env MYARGS="-name Example5-0 -GPU True -time 3600 -b 2.0 -a 1 -d fd -ID 0" < submit_gpu.sh
+mkdir -p outputs/Example1/Markdown
+bsub -o "outputs/Example1/Markdown/Example1_0.md" -J "Example1_0" -env MYARGS="-name Example1-0 -GPU False -time 3600 -b 2.0 -a 1 -d fd -ID 0" < submit_cpu.sh
