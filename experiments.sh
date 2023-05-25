41cba4c4a321aca8b6e89fb2989eaaf659574472
snow1 <15653507629@163.com>
2023-05-25 20:23:13 +0200

    LSTM1

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/Example4/Markdown
-bsub -o "outputs/Example4/Markdown/Example4_0.md" -J "Example4_0" -env MYARGS="-name Example4-0 -GPU False -time 360000 -b 2.0 -a 1 -d fd -ID 0" < submit_cpu.sh
+mkdir -p outputs/LSTMTest1/Markdown
+bsub -o "outputs/LSTMTest1/Markdown/LSTMTest1_0.md" -J "LSTMTest1_0" -env MYARGS="-name LSTMTest1-0 -GPU False -time 360000 -b 2.0 -a 1 -d fd -ID 0" < submit_cpu.sh
