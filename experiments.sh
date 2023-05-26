c2a1834c16ae2a54364ca560cc3bc2a53e8bc08e
snow1 <15653507629@163.com>
2023-05-26 11:54:18 +0200

    f

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mkdir -p outputs/transformerWithpretrain /Markdown
-bsub -o "outputs/transformerWithpretrain /Markdown/transformerWithpretrain _0.md" -J "transformerWithpretrain _0" -env MYARGS="-name transformerWithpretrain -0 -GPU False -time 360000 -model transformer -ID 0" < submit_cpu.sh
-bsub -o "outputs/transformerWithpretrain /Markdown/transformerWithpretrain _1.md" -J "transformerWithpretrain _1" -env MYARGS="-name transformerWithpretrain -1 -GPU False -time 360000 -model transformer -ID 1" < submit_cpu.sh
+mkdir -p outputs/transformerWithpretrain/Markdown
+bsub -o "outputs/transformerWithpretrain/Markdown/transformerWithpretrain_0.md" -J "transformerWithpretrain_0" -env MYARGS="-name transformerWithpretrain-0 -GPU False -time 360000 -model transformer -ID 0" < submit_cpu.sh
+bsub -o "outputs/transformerWithpretrain/Markdown/transformerWithpretrain_1.md" -J "transformerWithpretrain_1" -env MYARGS="-name transformerWithpretrain-1 -GPU False -time 360000 -model transformer -ID 1" < submit_cpu.sh
