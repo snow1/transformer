3d17a566a6a90dc01e504f88065a59158ede584d
snow1 <15653507629@163.com>
2023-05-28 15:22:14 +0200

    f

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/ACGAN2/Markdown
-bsub -o "outputs/ACGAN2/Markdown/ACGAN2_0.md" -J "ACGAN2_0" -env MYARGS="-name ACGAN2-0 -GPU False -time 360000 -model ACGAN -ID 0" < submit_cpu.sh
+mkdir -p outputs/ACGAN3/Markdown
+bsub -o "outputs/ACGAN3/Markdown/ACGAN3_0.md" -J "ACGAN3_0" -env MYARGS="-name ACGAN3-0 -GPU False -time 360000 -model ACGAN -ID 0" < submit_cpu.sh
