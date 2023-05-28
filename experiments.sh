44cd36abeae9615bddea6d5554541602cd2add12
snow1 <15653507629@163.com>
2023-05-28 12:24:51 +0200

    CWGAN2

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mkdir -p outputs/CWGAN/Markdown
-bsub -o "outputs/CWGAN/Markdown/CWGAN_0.md" -J "CWGAN_0" -env MYARGS="-name CWGAN-0 -GPU False -time 360000 -model CWGAN -ID 0" < submit_cpu.sh
-bsub -o "outputs/CWGAN/Markdown/CWGAN_1.md" -J "CWGAN_1" -env MYARGS="-name CWGAN-1 -GPU False -time 360000 -model CWGAN -ID 1" < submit_cpu.sh
+mkdir -p outputs/CWGAN2/Markdown
+bsub -o "outputs/CWGAN2/Markdown/CWGAN2_0.md" -J "CWGAN2_0" -env MYARGS="-name CWGAN2-0 -GPU False -time 360000 -model CWGAN -ID 0" < submit_cpu.sh
+bsub -o "outputs/CWGAN2/Markdown/CWGAN2_1.md" -J "CWGAN2_1" -env MYARGS="-name CWGAN2-1 -GPU False -time 360000 -model CWGAN -ID 1" < submit_cpu.sh
