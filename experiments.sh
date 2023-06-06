be9c5e668a26aced3bafd9e9058fc95f62b59fdd
snow1 <15653507629@163.com>
2023-06-06 23:05:04 +0200

    f

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-mkdir -p outputs/transformer2Test/Markdown
-bsub -o "outputs/transformer2Test/Markdown/transformer2Test_0.md" -J "transformer2Test_0" -env MYARGS="-name transformer2Test-0 -GPU False -time 360000 -model transformer2 -ID 0" < submit_cpu.sh
+mkdir -p outputs/transformerEMB_size5DEPTH2/Markdown
+bsub -o "outputs/transformerEMB_size5DEPTH2/Markdown/transformerEMB_size5DEPTH2_0.md" -J "transformerEMB_size5DEPTH2_0" -env MYARGS="-name transformerEMB_size5DEPTH2-0 -GPU False -time 360000 -model transformer -ID 0" < submit_cpu.sh
+bsub -o "outputs/transformerEMB_size5DEPTH2/Markdown/transformerEMB_size5DEPTH2_1.md" -J "transformerEMB_size5DEPTH2_1" -env MYARGS="-name transformerEMB_size5DEPTH2-1 -GPU False -time 360000 -model transformer -ID 1" < submit_cpu.sh
