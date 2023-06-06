c52dfdc5f6848237e257e4f14b851e1c0b4a8e52
snow1 <15653507629@163.com>
2023-06-06 20:42:46 +0200

    f

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 mkdir -p outputs/transformer2/Markdown
 bsub -o "outputs/transformer2/Markdown/transformer2_0.md" -J "transformer2_0" -env MYARGS="-name transformer2-0 -GPU False -time 360000 -model transformer2 -ID 0" < submit_cpu.sh
-bsub -o "outputs/transformer2/Markdown/transformer2_1.md" -J "transformer2_1" -env MYARGS="-name transformer2-1 -GPU False -time 360000 -model transformer2 -ID 1" < submit_cpu.sh
