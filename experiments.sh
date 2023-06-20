e54aa10839c6443029b6b2a572c73986611e34c2
snow1 <15653507629@163.com>
2023-06-20 19:21:08 +0200

    f

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p outputs/maintransformerBigdata/Markdown
-bsub -o "outputs/maintransformerBigdata/Markdown/maintransformerBigdata_0.md" -J "CNN_0" -env MYARGS="-name maintransformerBigdata-0 -GPU False -time 360000 -model maintransformer -ID 0" < submit_cpu.sh
-bsub -o "outputs/maintransformerBigdata/Markdown/maintransformerBigdata_1.md" -J "CNN_1" -env MYARGS="-name maintransformerBigdata-1 -GPU False -time 360000 -model maintransformer -ID 1" < submit_cpu.sh
+bsub -o "outputs/maintransformerBigdata/Markdown/maintransformerBigdata_0.md" -J "maintransformerBigdata_0" -env MYARGS="-name maintransformerBigdata-0 -GPU False -time 360000 -model maintransformer -ID 0" < submit_cpu.sh
+bsub -o "outputs/maintransformerBigdata/Markdown/maintransformerBigdata_1.md" -J "maintransformerBigdata_1" -env MYARGS="-name maintransformerBigdata-1 -GPU False -time 360000 -model maintransformer -ID 1" < submit_cpu.sh
