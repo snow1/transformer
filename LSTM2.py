825dd60a0a53d03e41ae7975c80a07490b9e90f0
snow1 <15653507629@163.com>
2023-06-21 21:10:16 +0200

    f

--- LSTM2.py
+++ LSTM2.py
@@ -1,10 +1,3 @@
-"""
-Transformer for EEG classification
-
-The core idea is slicing, which means to split the signal along the time dimension. Slice is just like the patch in Vision Transformer.
-"""
-
-import mne
 import os
 import numpy as np
 import math
@@ -14,7 +7,7 @@ import scipy.io
 
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
-#from torchsummary import summary
+from torchsummary import summary
 
 import torch
 import torch.nn.functional as F
@@ -25,240 +18,50 @@ from torch import Tensor
 from einops import rearrange, reduce, repeat
 from einops.layers.torch import Rearrange, Reduce
 from common_spatial_pattern import csp
-import pandas as pdss
-from sklearn.utils import shuffle
-from itertools import combinations
-# from confusion_matrix import plot_confusion_matrix
-# from cm_no_normal import plot_confusion_matrix_nn
-# from torchsummary import summary
 
-#import matplotlib.pyplot as plt
-# from torch.utils.tensorboard import SummaryWriter
-# from torch.backends import cudnn
-# cudnn.benchmark = False
-# cudnn.deterministic = True
 
-# writer = SummaryWriter('./TensorBoardX/')
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        # LSTM model 
+        # input_size: The number of expected features in the input `x`
+        # hidden_size: The number of features in the hidden state `h`
+        # num_layers: Number of recurrent layers. E.g., setting ``num_layers=2``
+        #     would mean stacking two LSTMs together to form a `stacked LSTM`,
+        #     with the second LSTM taking in outputs of the first LSTM and
+        #     computing the final results. Default: 1
+        # improve lstm
 
-# torch.cuda.set_device(6)
-# gpus = [0]
-# os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-# os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(map(str, gpus))
+        self.lstm = nn.LSTM(16, 10, 3, batch_first=True, bidirectional=True)
+        self.classify = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(20000, 2),
+            nn.Softmax(dim=1)
 
-class PatchEmbedding(nn.Module):
-    def __init__(self, emb_size):
-        # self.patch_size = patch_size
-        super().__init__()
-        self.projection = nn.Sequential(
-            nn.Conv2d(2, 2, (1, 51), (1, 1)),
-            nn.BatchNorm2d(2),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(2, emb_size, (16, 5), stride=(1, 5)),
-            Rearrange('b e (h) (w) -> b (h w) e'),
         )
-        self.cls_token = nn.Parameter(torch.randn(1, 1, emb_size))
-        # self.positions = nn.Parameter(torch.randn((100 + 1, emb_size)))
-        # self.positions = nn.Parameter(torch.randn((2200 + 1, emb_size)))
-
-    def forward(self, x: Tensor) -> Tensor:
-        b, _, _, _ = x.shape
-        x = self.projection(x)
-        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
-
-        # position
-        # x += self.positions
-        return x
-
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self, emb_size, num_heads, dropout):
-        super().__init__()
-        self.emb_size = emb_size
-        self.num_heads = num_heads
-        self.keys = nn.Linear(emb_size, emb_size)
-        self.queries = nn.Linear(emb_size, emb_size)
-        self.values = nn.Linear(emb_size, emb_size)
-        self.att_drop = nn.Dropout(dropout)
-        self.projection = nn.Linear(emb_size, emb_size)
-
-    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
-        queries = rearrange(self.queries(x), "b n (h d) -> b h n d", h=self.num_heads)
-        keys = rearrange(self.keys(x), "b n (h d) -> b h n d", h=self.num_heads)
-        values = rearrange(self.values(x), "b n (h d) -> b h n d", h=self.num_heads)
-        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)  # batch, num_heads, query_len, key_len
-        if mask is not None:
-            fill_value = torch.finfo(torch.float32).min
-            energy.mask_fill(~mask, fill_value)
-
-        scaling = self.emb_size ** (1 / 2)
-        att = F.softmax(energy / scaling, dim=-1)
-        att = self.att_drop(att)
-        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
-        out = rearrange(out, "b h n d -> b n (h d)")
-        out = self.projection(out)
-        return out
-
-
-class ResidualAdd(nn.Module):
-    def __init__(self, fn):
-        super().__init__()
-        self.fn = fn
-
-    def forward(self, x, **kwargs):
-        res = x
-        x = self.fn(x, **kwargs)
-        x += res
-        return x
-
-
-class FeedForwardBlock(nn.Sequential):
-    def __init__(self, emb_size, expansion, drop_p):
-        super().__init__(
-            nn.Linear(emb_size, expansion * emb_size),
-            nn.GELU(),
-            nn.Dropout(drop_p),
-            nn.Linear(expansion * emb_size, emb_size),
-        )
-
-class GELU(nn.Module):
-    def forward(self, input: Tensor) -> Tensor:
-        return input*0.5*(1.0+torch.erf(input/math.sqrt(2.0)))
-
-
-class TransformerEncoderBlock(nn.Sequential):
-    def __init__(self,
-                 emb_size,
-                 num_heads=5,
-                 drop_p=0.5,
-                 forward_expansion=4,
-                 forward_drop_p=0.5):
-        super().__init__(
-            ResidualAdd(nn.Sequential(
-                nn.LayerNorm(emb_size),
-                MultiHeadAttention(emb_size, num_heads, drop_p),
-                nn.Dropout(drop_p)
-            )),
-            ResidualAdd(nn.Sequential(
-                nn.LayerNorm(emb_size),
-                FeedForwardBlock(
-                    emb_size, expansion=forward_expansion, drop_p=forward_drop_p),
-                nn.Dropout(drop_p)
-            )
-            ))
-
-
-class TransformerEncoder(nn.Sequential):
-    def __init__(self, depth, emb_size):
-        super().__init__(*[TransformerEncoderBlock(emb_size) for _ in range(depth)])
-
-
-class ClassificationHead(nn.Sequential):
-    def __init__(self, emb_size, n_classes):
-        super().__init__()
-        self.clshead = nn.Sequential(
-            Reduce('b n e -> b e', reduction='mean'),
-            nn.LayerNorm(emb_size),
-            nn.Linear(emb_size, n_classes)
-        )
-
+    
     def forward(self, x):
-        out = self.clshead(x)
-        return x, out
+        #print("1", x.shape) # 50,1 16,1000
+        x = x.squeeze(1)
+        #print("2", x.shape) # 50, 16, 1000
+        x = x.permute(0, 2, 1)
+        #print("3", x.shape) # 50, 1000, 16
+        x, _ = self.lstm(x)
+        #print("4", x.shape) # 50 1000 20
+        x = self.classify(x)
+        #print("5",x.shape) # 50 4
+        return x
+            
 
 
-class ViT(nn.Sequential):
-    def __init__(self, emb_size=10, depth=3, n_classes=2, **kwargs):
-    #def __init__(self, emb_size=5, depth=2, n_classes=2, **kwargs):
-
-        super().__init__(
-            # channel_attention(),
-            ResidualAdd(
-                nn.Sequential(
-                    nn.LayerNorm(3000),
-                    channel_attention(),
-                    nn.Dropout(0.5),
-                )
-            ),
-
-            PatchEmbedding(emb_size),
-            TransformerEncoder(depth, emb_size),
-            ClassificationHead(emb_size, n_classes)
-        )
-
-
-class channel_attention(nn.Module):
-    def __init__(self, sequence_num=3000, inter=30):
-        super(channel_attention, self).__init__()
-        self.sequence_num = sequence_num
-        self.inter = inter
-        self.extract_sequence = int(self.sequence_num / self.inter)  # You could choose to do that for less computation
-
-        self.query = nn.Sequential(
-            nn.Linear(16, 16), #32*32
-            nn.LayerNorm(16),  # also may introduce improvement to a certain extent
-            nn.Dropout(0.3)
-        )
-        self.key = nn.Sequential(
-            nn.Linear(16, 16),
-            # result2
-            #nn.LeakyReLU(),
-
-            nn.LayerNorm(16),
-            nn.Dropout(0.3)
-        )
-
-        # self.value = self.key
-        self.projection = nn.Sequential(
-            nn.Linear(16, 16),
-            #result2
-            #nn.LeakyReLU(),
-
-            nn.LayerNorm(16),
-            nn.Dropout(0.3),
-        )
-
-        self.drop_out = nn.Dropout(0)
-        self.pooling = nn.AvgPool2d(kernel_size=(1, self.inter), stride=(1, self.inter))
-
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0.0)
-
-    def forward(self, x):
-        temp = rearrange(x, 'b o c s->b o s c')
-        temp_query = rearrange(self.query(temp), 'b o s c -> b o c s')
-        temp_key = rearrange(self.key(temp), 'b o s c -> b o c s')
-
-        channel_query = self.pooling(temp_query)
-        channel_key = self.pooling(temp_key)
-
-        scaling = self.extract_sequence ** (1 / 2)
-
-        channel_atten = torch.einsum('b o c s, b o m s -> b o c m', channel_query, channel_key) / scaling
-
-        channel_atten_score = F.softmax(channel_atten, dim=-1)
-        channel_atten_score = self.drop_out(channel_atten_score)
-
-        out = torch.einsum('b o c s, b o c m -> b o c s', x, channel_atten_score)
-        '''
-        projections after or before multiplying with attention score are almost the same.
-        '''
-        out = rearrange(out, 'b o c s -> b o s c')
-        out = self.projection(out)
-        out = rearrange(out, 'b o s c -> b o c s')
-        return out
-
-
-class Trans():
-    def __init__(self, nsub: int):
-        super(Trans, self).__init__()
-        self.batch_size = 1 
+class LSTM:
+    def __init__(self, nsub):
+        super(LSTM, self).__init__()
+        self.batch_size = 50
         self.n_epochs = 1000
         self.img_height = 22
         self.img_width = 600
-        self.channels = 22 # EEG channels 22      # 1
+        self.channels = 1
         self.c_dim = 4
         self.lr = 0.0002
         self.b1 = 0.5
@@ -269,7 +72,7 @@ class Trans():
 
         self.pretrain = False
 
-        self.log_write = open("./results2/log_subject%d.txt" % self.nSub, "w")
+        self.log_write = open("./LSTMresults/log_subject%d.txt" % self.nSub, "w")
 
         self.img_shape = (self.channels, self.img_height, self.img_width)  # something no use
 
@@ -280,23 +83,19 @@ class Trans():
         self.criterion_l2 = torch.nn.MSELoss()
         self.criterion_cls = torch.nn.CrossEntropyLoss()
 
-        self.model = ViT()
-        if self.pretrain:
-            self.model.load_state_dict(torch.load(f'./models/model_%d.pth' % self.nSub))
-
-        # self.model = nn.DataParallel(self.model, device_ids=[i for i in range(len(gpus))])
+        self.model = Model()
+        #self.model = nn.DataParallel(self.model, device_ids=[i for i in range(len(gpus))])
         # self.model = self.model
-        #summary(self.model, (1, 16, 1000), device='cpu')
+       # summary(self.model, (16,16), device='cpu')
 
         self.centers = {}
-   
+
     def get_source_data(self):
 
         # to get the data of target subject
         self.total_data = scipy.io.loadmat(self.root + 'data/mat/A0%dT.mat' % self.nSub)
         self.train_data = self.total_data['data']
         self.train_label = self.total_data['label']
-        
 
         self.train_data = np.transpose(self.train_data, (2, 1, 0))
         self.train_data = np.expand_dims(self.train_data, axis=1)
@@ -330,7 +129,6 @@ class Trans():
         self.allData = np.einsum('abcd, ce -> abed', self.allData, Wb)
         self.testData = np.einsum('abcd, ce -> abed', self.testData, Wb)
         return self.allData, self.allLabel, self.testData, self.testLabel
-    
     def normalize_vector(self,vector):
         normalized_vector = (vector - vector.min()) / (vector.max() - vector.min())
         return normalized_vector
@@ -526,34 +324,29 @@ class Trans():
         curr_lr = self.lr
         # some better optimization strategy is worthy to explore. Sometimes terrible over-fitting.
 
-        print('Training...')
-        # print(img.shape) #[180, 2, 3000, 1]
-        # print(label.shape) [180]
+
         for e in range(self.n_epochs):
             in_epoch = time.time()
             self.model.train()
             for i, (img, label) in enumerate(self.dataloader):
+
                 img = Variable(img.type(self.Tensor))
                 label = Variable(label.type(self.LongTensor))
-                tok, outputs = self.model(img)
-                #(outputs.shape)
-
+                outputs = self.model(img)               
+               # print(outputs.shape)
                 loss = self.criterion_cls(outputs, label)
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
             out_epoch = time.time()
-            
-            #TEST
+
             if (e + 1) % 1 == 0:
                 self.model.eval()
-                Tok, Cls = self.model(test_data)
-                #print(Cls)
-                #print(test_label)
+                Cls = self.model(test_data)
+
                 loss_test = self.criterion_cls(Cls, test_label)
                 y_pred = torch.max(Cls, 1)[1]
-                #print(y_pred)
                 acc = float((y_pred == test_label).cpu().numpy().astype(int).sum()) / float(test_label.size(0))
                 train_pred = torch.max(outputs, 1)[1]
                 train_acc = float((train_pred == label).cpu().numpy().astype(int).sum()) / float(label.size(0))
@@ -561,9 +354,7 @@ class Trans():
                       '  Train loss:', loss.detach().cpu().numpy(),
                       '  Test loss:', loss_test.detach().cpu().numpy(),
                       '  Train accuracy:', train_acc,
-                      '  Test accuracy is:', acc,
-                      '  Train_predict:', train_pred,
-                      '  label:', label)
+                      '  Test accuracy is:', acc)
                 self.log_write.write(str(e) + "    " + str(acc) + "\n")
                 num = num + 1
                 averAcc = averAcc + acc
@@ -572,7 +363,7 @@ class Trans():
                     Y_true = test_label
                     Y_pred = y_pred
 
-        torch.save(self.model.state_dict(), f'models2/model_{self.nSub}.pth')
+        torch.save(self.model.state_dict(), f'LSTMModels/model_{self.nSub}.pth')
         averAcc = averAcc / num
         print('The average accuracy is:', averAcc)
         print('The best accuracy is:', bestAcc)
@@ -580,7 +371,6 @@ class Trans():
         self.log_write.write('The best accuracy is: ' + str(bestAcc) + "\n")
 
         return bestAcc, averAcc, Y_true, Y_pred
-
 
 def main():
     best = 0
@@ -619,6 +409,7 @@ def main():
     result_write.write('**The average Best accuracy is: ' + str(best) + "\n")
     result_write.write('The average Aver accuracy is: ' + str(aver) + "\n")
     result_write.close()
+
 
 
 if __name__ == "__main__":
--- experiments.sh
+++ experiments.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/transformer4/Markdown
-bsub -o "outputs/transformer4/Markdown/transformer4_0.md" -J "transformer4_0" -env MYARGS="-name transformer4-0 -GPU False -time 360000 -model transformer4 -ID 0" < submit_cpu.sh
-bsub -o "outputs/transformer4/Markdown/transformer4_1.md" -J "transformer4_1" -env MYARGS="-name transformer4-1 -GPU False -time 360000 -model transformer4 -ID 1" < submit_cpu.sh
-mkdir -p outputs/transformer5/Markdown
-bsub -o "outputs/transformer5/Markdown/transformer5_0.md" -J "transformer5_0" -env MYARGS="-name transformer5-0 -GPU False -time 360000 -model transformer5 -ID 0" < submit_cpu.sh
-bsub -o "outputs/transformer5/Markdown/transformer5_1.md" -J "transformer5_1" -env MYARGS="-name transformer5-1 -GPU False -time 360000 -model transformer5 -ID 1" < submit_cpu.sh
+mkdir -p outputs/transformer5To1/Markdown
+bsub -o "outputs/transformer5To1/Markdown/transformer5To1_0.md" -J "transformer5To1_0" -env MYARGS="-name transformer5To1-0 -GPU False -time 360000 -model transformer5 -ID 0" < submit_cpu.sh
