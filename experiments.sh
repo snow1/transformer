45225987242ef265e07cbbaa7fa593d71a4d46d4
snow1 <15653507629@163.com>
2023-06-30 17:19:46 +0200

    f

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/transformer5To1/Markdown
-bsub -o "outputs/transformer5To1/Markdown/transformer5To1_0.md" -J "transformer5To1_0" -env MYARGS="-name transformer5To1-0 -GPU False -time 360000 -model transformer5 -ID 0" < submit_cpu.sh
+mkdir -p outputs/transformer6Final/Markdown
+bsub -o "outputs/transformer6Final/Markdown/transformer6Final_0.md" -J "transformer6Final_0" -env MYARGS="-name transformer6Final-0 -GPU True -time 360000 -model transformer5 -ID 0" < submit_gpu.sh
--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ from main import Defaults
 #Defaults("LSTMTest2withSoftmax",GPU=False, model="lstm", instances=1)
 #Defaults("linearTest1",GPU=False, model="linear", instances=1)
 #Defaults("transformer4",GPU=False, model="transformer4", instances=2)
-Defaults("transformer5To1",GPU=False, model="transformer5", instances=1)
+Defaults("transformer6Final",GPU=True, model="transformer5", instances=1)
 
 #Defaults("Example2", b=4, d="dssf", GPU=True)
 #Defaults("Example3", a=2, instances=2)linear
