9587591c4da9515697c458245f042a351ed2c20b
snow1 <15653507629@163.com>
2023-07-10 23:46:57 +0200

    f

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/transformer6Final2with109peopelCnanelfrom0to64/Markdown
-bsub -o "outputs/transformer6Final2with109peopelCnanelfrom0to64/Markdown/transformer6Final2with109peopelCnanelfrom0to64_0.md" -J "transformer6Final2with109peopelCnanelfrom0to64_0" -env MYARGS="-name transformer6Final2with109peopelCnanelfrom0to64-0 -GPU True -time 360000 -model transformer6 -ID 0" < submit_gpu.sh
+mkdir -p outputs/transformer44444/Markdown
+bsub -o "outputs/transformer44444/Markdown/transformer44444_0.md" -J "transformer44444_0" -env MYARGS="-name transformer44444-0 -GPU False -time 360000 -model transformer4 -ID 0" < submit_cpu.sh
